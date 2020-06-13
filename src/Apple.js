db8a8df7327ee3dc08bda5082132ea067f023fc1
Barnit Basnet <barnit.basnet1@gmail.com>
2020-06-13 22:36:23 +0545

    User Media Mode

--- src/Apple.js
+++ src/Apple.js
@@ -1,10 +1,30 @@
-import React, { useState } from 'react';
+import React, { useState ,useEffect} from 'react';
 import './App.css';
 
 function Apple() {
-  const [dark ,setdark] = useState(false)
+  
+  const getMode = () => {
+    const initialMode = localStorage.getItem('mode')
+    if (initialMode==null){
+      if(window.matchMedia("(prefers-color-scheme:dark)").matches){
+        return true 
+      }
+      else{
+        return false
+      }
+    }else{
+      return JSON.parse(initialMode)
+    }
+  }
+  //this way u can check user preference of mode 
 
- 
+  const [dark ,setdark] = useState(getMode()); 
+  
+  
+  useEffect(() => {
+     localStorage.setItem("mode" , JSON.stringify(dark))
+  }, [dark]);
+
 
   return (
     <div className={dark?"App dark-mode" : "App"}>
@@ -12,6 +32,7 @@ function Apple() {
         <label class ="switch">
           <input 
           type ="checkbox"
+          checked = {dark}
           onChange={() => setdark(!dark)}/>
           <span class = "slider round"></span>
         
